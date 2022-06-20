f5283ae5009a6c13306d87534e9907d21e35f621
Angelina Polyakova <lina.krasilnikova.1996@mail.ru>
2022-06-20 21:25:44 +0400

    HTML and css page

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let hideBtn = document.querySelector("#hide-btn");
+let desicionsItems = document.querySelector("#desicionsItems");
+
+hideBtn.addEventListener("click", function () {
+  desicionsItems.style.display = "none";
+});
